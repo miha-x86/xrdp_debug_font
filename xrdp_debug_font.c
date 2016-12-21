72f8fe2b30231720e99d29c0e2ac2487f713b95e
miha-x86 <miha-x86@users.noreply.github.com>
2016-12-21 11:59:20 -0200

    total rewrite

--- xrdp_debug_font.c
+++ xrdp_debug_font.c
@@ -6,235 +6,188 @@
 #include <errno.h>
 #include <string.h>
 #include <stdlib.h>
+#include <ctype.h>
+#include <tesseract/capi.h>
+#include <leptonica/allheaders.h>
+#include <errno.h>
 
 #define FONT_DATASIZE(_w, _h) (((_h * ((_w + 7) / 8)) + 3) & ~3)
 
-static int check_header(int fd) {
+typedef enum { false, true } bool;
+typedef int               int32;
+typedef unsigned int      uint32;
+typedef unsigned short    ushort;
+typedef unsigned char     byte;
 
-    unsigned char buffer[128];
+typedef struct __attribute__((__packed__)) {
+    short width;
+    short height;
+    short baseline;
+    short offset;
+    short incby;
+    /* 6 bytes of padding here */
+    byte* data;
+    int   size;
+} xrdp_glyph;
 
-    /* Header string */
-    read(fd, buffer, 4);
-    if (!memcmp(buffer, "FNT1", 4))
-        printf("Header OK!\n");
-    else
-        printf("Header Error!\n");
+typedef struct __attribute__((__packed__)) {
+    char        header[4];
+    char        name[32];
+    short       size;
+    short       style;
+    /* 8 bytes of padding here */
+    xrdp_glyph* glyphs;
+} xrdp_font;
 
-    /* Font Family */
-    read(fd, buffer, 32);
-    buffer[32] = 0;
-    printf("Font Family: %s\n", buffer);
+#define READ_BYTES(fd, buffer, size) {          \
+    int r;                                      \
+    r = fdread(fd, buffer, size);               \
+    if (r == 0)                                 \
+        return 1;                               \
+    if (r == -1) {                              \
+        printf("Error: %s:%i %s\n", __FILE__, __LINE__, strerror(errno)); \
+        return 1;                                                       \
+    }                                                                   \
+    }
 
-    /* Font size */
-    read(fd, buffer, 2);
-    printf("Font Size: %i\n", *((short*) buffer));
+int
+fdread(int   fd,
+       void* buffer,
+       int   size)
+{
+    int r;
+    int bytes_read;
 
-    /* Font style */
-    read(fd, buffer, 2);
-    printf("Font style: %i\n", *((short*) buffer));
-
-    /* Read padding */
-    read(fd, buffer, 8);
-
-    return 0;
-
+    r = read(fd, buffer, size);
+    bytes_read = r;
+    while (r > 0 && bytes_read < size) {
+        r = read(fd, buffer + bytes_read, size);
+        bytes_read += r;
+    }
+    return r;
 }
 
-static int read_glyph(int fd, char stop) {
+static void
+unload_font(xrdp_font* font)
+{
+    int i;
 
-    unsigned char buffer[512];
-    short w, h;
-    int r;
-    static int index = 32;
+    for (i = 0; i < 0x4e00 - 32; i++)
+        free(font->glyphs[i].data);
+    free(font->glyphs);
+    free(font);
+}
 
-    /* Width */
-    printf("\twidth: ");
-    r = read(fd, buffer, 2);
-    if (r == -1 || !r) {
-        if (r == -1)
-            printf("Error: %s.\n", strerror(errno));
-        else
-            printf("Unexpected end of file.\n");
+static int
+load_glyphs(int        fd,
+            xrdp_font* font)
+{
+    int         i, r;
+    xrdp_glyph* glyph;
+    byte        dummy[10];
 
-        return r;
+    for (i = 0; i < 0x4e00 - 32; i++) {
+        glyph = font->glyphs + i;
+
+        /* Glyph info */
+        READ_BYTES(fd, glyph, sizeof(xrdp_glyph) - sizeof(byte*) - sizeof(int));
+
+        /* Padding */
+        READ_BYTES(fd, dummy, 6);
+
+        /* Data */
+        glyph->size = FONT_DATASIZE(glyph->width, glyph->height);
+        if (!glyph->size)
+            continue;
+
+        glyph->data = malloc(glyph->size);
+        READ_BYTES(fd, glyph->data, glyph->size);
     }
-    w = *((short*) buffer);
-    printf("%i\n", w);
+    return 0;
+}
 
-    /* Height */
-    printf("\theight: ");
-    r = read(fd, buffer, 2);
-    if (r == -1 || !r) {
-        if (r == -1)
-            printf("Error: %s.\n", strerror(errno));
-        else
-            printf("Unexpected end of file.\n");
+static xrdp_font*
+load_font(const char* filename)
+{
+    xrdp_font* font;
+    int        fd;
+    byte       dummy[10];
 
-        return r;
+    fd = open(filename, O_RDONLY);
+    if (fd == -1) {
+        printf("Error: %s.\n", strerror(errno));
+        return NULL;
     }
-    h = *((short*) buffer);
-    printf("%i\n", h);
 
-    /* Baseline */
-    printf("\tbaseline: ");
-    r = read(fd, buffer, 2);
-    if (r == -1 || !r) {
-        if (r == -1)
-            printf("Error: %s.\n", strerror(errno));
-        else
-            printf("Unexpected end of file.\n");
+    font = calloc(1, sizeof(xrdp_font));
+    if (!font)
+        return NULL;
 
-        return r;
+    /* Alloc glyph space */
+    font->glyphs = calloc(1, sizeof(xrdp_glyph) * (0x4e00 - 32));
+    if (!font->glyphs) {
+        unload_font(font);
+        return NULL;
     }
-    printf("%i\n", *((short*) buffer));
 
-    /* Offset */
-    printf("\toffset: ");
-    r = read(fd, buffer, 2);
-    if (r == -1 || !r) {
-        if (r == -1)
-            printf("Error: %s.\n", strerror(errno));
-        else
-            printf("Unexpected end of file.\n");
-
-        return r;
-    }
-    printf("%i\n", *((short*) buffer));
-
-    /* Incby */
-    printf("\tincby: ");
-    r = read(fd, buffer, 2);
-    if (r == -1 || !r) {
-        if (r == -1)
-            printf("Error: %s.\n", strerror(errno));
-        else
-            printf("Unexpected end of file.\n");
-
-        return r;
-    }
-    printf("%i\n", *((short*) buffer));
+    /* Font info */
+    fdread(fd, font, sizeof(xrdp_font) - sizeof(xrdp_glyph*));
 
     /* Padding */
-    r = read(fd, buffer, 6);
-    if (r == -1 || !r) {
-        if (r == -1)
-            printf("Error: %s.\n", strerror(errno));
-        else
-            printf("Unexpected end of file.\n");
+    fdread(fd, dummy, 8);
 
-        return r;
+    if (load_glyphs(fd, font)) {
+        unload_font(font);
+        return NULL;
     }
 
-    /* Glyph Character */
-    if (isgraph(index))
-        printf("\tchar: '%c' %i\n", index, index);
-    else
-        printf("\tchar: '%s' %i\n", "n/a", index);
-
-    /* Glyph Data */
-    int glyph_size = FONT_DATASIZE(w, h);
-
-    printf("\tglyph data (%i bytes):", glyph_size);
-    r = read(fd, buffer, glyph_size);
-    if (r == -1) {
-        printf("Error: %s.\n", strerror(errno));
-        return r;
-    }
-
-    int _w = (w + 7) & ~7;
-    int roller = 0;
-    int i = 0;
-    int j = 1;
-
-    while (i < glyph_size) {
-
-        if ((roller % _w) == 0)
-            printf("\n\t%*i: ", 2, j++);
-
-        printf("%c", (buffer[i] & (0x80 >> (roller & 7))) ? '1' : '0');
-
-        roller++;
-        if ((roller & 7) == 0)
-            i++;
-
-    }
-    printf("\n\n");
-
-    /* Stop char reached */
-    if (index == stop && stop)
-        return -2;
-
-    index++;
-
-    return r;
-
+    return font;
 }
 
 
-static int check_glyphs(int fd, char stop) {
+static void
+print_glyph(xrdp_glyph* glyph)
+{
+    int  w, h, x, y, i;
+    byte b, *data;
 
-    int r;
+    w = (glyph->width + 7) & ~7;
+    h = (glyph->height + 3) & ~3;
+    data = glyph->data;
 
-    printf("Glyphs:\n");
-    if (stop)
-        printf("Stop char: '%c'\n", stop);
-
-    while (1) {
-
-        /* Read glyph */
-        r = read_glyph(fd, stop);
-
-        /* Error */
-        if (r == -1)
-            return 1;
-
-        /* Stopped at stop char */
-        if (r == -2)
-            return 0;
-
+    for (y = 0; y < h; y++) {
+        b = *data++;
+        for (x = 0; x < w;) {
+            for (i = 0; i < 8 && x < w; i++, x++) {
+                if (b & 0x80)
+                    putchar('1');
+                else
+                    putchar('0');
+                b <<= 1;
+            }
+        }
+        putchar('\n');
     }
-
-    return 0;
-
 }
 
-int main(int argc, char** argv) {
+int
+main(int    argc,
+     char** argv)
+{
+    xrdp_font* font;
 
     if (argc < 2) {
         printf("Usage: %s <fontfile> [stop char]\n", argv[0]);
         return 1;
     }
 
-#if 0
-    if (!strcmp(argv[1], "--dump-ascii-charset")) {
-        int i;
+    printf("Loading font...\n");
+    font = load_font(argv[1]);
+    if (!font)
+        return 1;
+    printf("OK\n");
 
-        printf("charset = \"");
-        for (i=32; i<256; i++) {
-            if (i == 127)
-                continue;
-            printf("%c", i);
-        }
-        printf("\";\n");
-
-        return 0;
-    }
-#endif
-
-    int fd = open(argv[1], O_RDONLY);
-    if (fd == -1) {
-        printf("Error: %s.\n", strerror(errno));
-        return 2;
-    }
-
-    char c = NULL;
-    if (argc > 2)
-        c = argv[2][0];
-
-    check_header(fd);
-    check_glyphs(fd, c);
-
-    close(fd);
+    unload_font(font);
 
     return 0;
 
